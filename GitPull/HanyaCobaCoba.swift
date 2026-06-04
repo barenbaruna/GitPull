e5dc4839a83d1f316f302d86610dc148c5ae44d1
Bong <bpefando@gmail.com>
2026-06-04 11:00:14 +0700

    add HanyaCobaCoba

---
+++ GitPull/HanyaCobaCoba.swift
@@ -0,0 +1,18 @@
+//
+//  HanyaCobaCoba.swift
+//  GitPull
+//
+//  Created by BigMac on 04/06/26.
+//
+
+import SwiftUI
+
+struct HanyaCobaCoba: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HanyaCobaCoba()
+}
