4265454b3122e46628cea82cc5c959f3a47a0334
Bong <bpefando@gmail.com>
2026-06-04 11:51:44 +0700

    slash

--- GitPull/HanyaCobaCoba.swift
+++ GitPull/HanyaCobaCoba.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct HanyaCobaCoba: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("hanya coba coba")
-        Text("Untuk Pull Req")
-        
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        Text("hanya coba coba")
+//        Text("Untuk Pull Req")
+//        
         Text("Buat Testing Fitur Verify")
     }
 }
