9dd6f8f875bedce8ffacbf5f17c74b75cf5aeaec
Anita Rutmala <rutmalaanita@gmail.com>
2026-06-04 11:07:51 +0700

    Add Anita View

---
+++ GitPull/AnitaView.swift
@@ -0,0 +1,18 @@
+//
+//  AnitaView.swift
+//  GitPull
+//
+//  Created by Anita Rutmala on 04/06/26.
+//
+
+import SwiftUI
+
+struct AnitaView: View {
+    var body: some View {
+        Text("Hello Anita!!!")
+    }
+}
+
+#Preview {
+    AnitaView()
+}
