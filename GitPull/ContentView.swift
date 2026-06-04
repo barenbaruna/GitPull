840cc1a621f493f984a0b99253cf4e591f72a48d
Baren Baruna Harahap <barenbarunaharahap@gmail.com>
2026-06-04 10:38:16 +0700

    Initial Commit

---
+++ GitPull/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitPull
+//
+//  Created by Baren Baruna Harahap on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitPull/GitPullApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPullApp.swift
+//  GitPull
+//
+//  Created by Baren Baruna Harahap on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GitPullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
