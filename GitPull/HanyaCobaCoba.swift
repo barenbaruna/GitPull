c1c39d1c953b5249707f8bb343300bee35d7aded
Bong <bpefando@gmail.com>
2026-06-04 11:36:53 +0700

    Untuk testing verifikasi

--- GitPull/HanyaCobaCoba.swift
+++ GitPull/HanyaCobaCoba.swift
@@ -12,6 +12,8 @@ struct HanyaCobaCoba: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Text("hanya coba coba")
         Text("Untuk Pull Req")
+        
+        Text("Buat Testing Fitur Verify")
     }
 }
 
